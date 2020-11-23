059c745d0a6aa392492505f6f3c58f34dfd42d79
vpaklatzis <vpaklatzis@gmail.com>
2020-11-23 18:45:29 +0200

    Added basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { from } from 'rxjs';
-import 'hammerjs';
-import { MenuComponent } from './menu/menu.component';
+
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { DishdetailComponent } from './dishdetail/dishdetail.component'; // Task 1
+
+import 'hammerjs';
 import { DishService } from './services/dish.service';
+
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component'; // Task 1
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +29,10 @@ import { FooterComponent } from './footer/footer.component';
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent // Task 1
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent // Task 1
   ],
   imports: [
     BrowserModule,
