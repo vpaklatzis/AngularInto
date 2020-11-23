85b7643f2e703a0d9d7e2ecb57ec5516afac59c3
vpaklatzis <vpaklatzis@gmail.com>
2020-11-23 20:21:54 +0200

    Single page application part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,9 +14,10 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { MenuComponent } from './menu/menu.component';
-import { DishdetailComponent } from './dishdetail/dishdetail.component'; // Task 1
+import { DishdetailComponent } from './dishdetail/dishdetail.component'; 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
@@ -32,7 +33,7 @@ import { ContactComponent } from './contact/contact.component';
     FooterComponent,
     HomeComponent,
     AboutComponent,
-    ContactComponent // Task 1
+    ContactComponent 
   ],
   imports: [
     BrowserModule,
@@ -46,7 +47,8 @@ import { ContactComponent } from './contact/contact.component';
     MatButtonModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
