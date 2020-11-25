821c5d0ce87a010bc909f3daff84308e40002e19
vpaklatzis <vpaklatzis@gmail.com>
2020-11-25 17:34:29 +0200

    Added promises part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -16,8 +16,8 @@ export class DishdetailComponent implements OnInit {
   constructor(private dishService: DishService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
-    let id = this.route.snapshot.params['id'];
-    this.dish = this.dishService.getDish(id);
+    const id = this.route.snapshot.params['id'];
+    this.dishService.getDish(id).then(dish => this.dish = dish);
   }
 
   goBack(): void {
